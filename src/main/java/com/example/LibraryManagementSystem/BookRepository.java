d8ae0f5daf5b2d314289d65f90be4c9cdfb21385
gautam004 <gautam.cs16@gmail.com>
2020-09-26 01:02:24 +0530

    added REST services for fethcing and searching books in the system

--- src/main/java/com/example/LibraryManagementSystem/BookRepository.java
+++ src/main/java/com/example/LibraryManagementSystem/BookRepository.java
@@ -3,10 +3,20 @@ package com.example.LibraryManagementSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Books, Integer> {
 
-    @Query(value = "select * from Books b where b.bookCode=:bookCode", nativeQuery = true)
+    @Query(value = "select * from books b where b.book_Code=:bookCode", nativeQuery = true)
+    public Books findByBookCode(String bookCode);
 
+    @Query(value = "select * from books b where b.book_author=:author", nativeQuery = true)
+    public List<Books> findBookByAuthor(String author);
 
+    @Query(value = "select * from books b where b.book_title=:title", nativeQuery = true)
+    public List<Books> findBookByTitle(String title);
+
+    @Query(value = "select * from books", nativeQuery = true)
+    public List<Books> fetchAllBooks();
 
 }
--- src/main/java/com/example/LibraryManagementSystem/BooksController.java
+++ src/main/java/com/example/LibraryManagementSystem/BooksController.java
@@ -3,8 +3,10 @@ package com.example.LibraryManagementSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -13,16 +15,27 @@ public class BooksController {
     @Autowired
     BookRepository repository;
 
-    @GetMapping("/books/{bookCode}")
-    public Books findBookByCode(@PathVariable int bookCode){
-
-        Optional<Books> object = repository.findById(bookCode);
-        if(object.isPresent()){
-            return object.get();
-        }
-        System.out.println("Could not find the book");
-        return null;
-
+    //search book by bookCode
+    @GetMapping("/books/bookCode/{bookCode}")
+    public Books findBookByCode(@PathVariable String bookCode){
+        return repository.findByBookCode(bookCode);
     }
 
+    //search book by author
+    @GetMapping("/books/author/{author}")
+    public List<Books> findBookByAuthor(@PathVariable String author){
+        return repository.findBookByAuthor(author);
+    }
+
+    //search book by title
+    @GetMapping("/books/title/{title}")
+    public List<Books> findBookByTitle(@PathVariable String title){
+        return repository.findBookByTitle(title);
+    }
+
+    //fetch all books
+    @GetMapping("/books")
+    public List<Books> fetchAllBooks(){
+        return repository.fetchAllBooks();
+    }
 }
