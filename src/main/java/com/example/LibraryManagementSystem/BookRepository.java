6dadcc1f7beb7f169c82d3c76bca5a7c7920a73f
gautam004 <gautam.cs16@gmail.com>
2020-09-25 23:52:05 +0530

    added method for fetching books from DB using REST

--- src/main/java/com/example/LibraryManagementSystem/BookRepository.java
+++ src/main/java/com/example/LibraryManagementSystem/BookRepository.java
@@ -1,8 +1,12 @@
 package com.example.LibraryManagementSystem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Books, Integer> {
+
+    @Query(value = "select * from Books b where b.bookCode=:bookCode", nativeQuery = true)
+
 
 
 }
